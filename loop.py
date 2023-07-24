ac3225a5593775bce967574cd8a0ec5224679af1
SomeName2 <none>
2023-07-24 14:47:51 +0200

    start time independent synchronization, reduced noise from audio synchronization

--- loop.py
+++ loop.py
@@ -34,48 +34,65 @@ def load_wave(file_path):
 	
 	return file_buffer, sample_width, num_channels, sample_rate
 
+def sleep_until(target_time):
+	sleep(max(target_time - time(), 0))
+
 def do_visual(port, sleep_duration, offset, start_time):
-	target_time = start_time
+	target_time = start_time + 2 * sleep_duration + offset
+	sleep_until(target_time)
 	
-	if(offset >= 0):
-		target_time += offset
-		sleep(target_time - time())
-		
+	while(True):
 		port.break_condition = True
 		
 		target_time += sleep_duration
-		sleep(max(target_time - time(), 0))
-	else:
-		target_time += sleep_duration + offset
-		sleep(max(target_time - time(), 0))
-	
-	while(True):
+		sleep_until(target_time)
+		
 		port.break_condition = False
 		
 		target_time += sleep_duration
-		sleep(target_time - time())
+		sleep_until(target_time)
+
+
+def adjust_start_i(file_buffer_np, last_i, start_i):
+	val = file_buffer_np[last_i]
+	grad_sign = np.sign(file_buffer_np[last_i + 1] - val)
+	
+	buffer_start_i = file_buffer_np[start_i]
+	buffer_start_i_1 = file_buffer_np[start_i + 1]
+	
+	next_grad = buffer_start_i_1 - buffer_start_i
+	last_abs_diff = abs(val - buffer_start_i)
+	next_abs_diff = abs(val - buffer_start_i_1)
+	
+	while np.sign(next_grad) != grad_sign or next_abs_diff < last_abs_diff:
+		start_i += 1
 		
-		port.break_condition = True
-		
-		target_time += sleep_duration
-		sleep(target_time - time())
-		
-		
+		buffer_start_i_1 = file_buffer_np[start_i + 1]
+	
+		next_grad = buffer_start_i_1 - file_buffer_np[start_i]
+		last_abs_diff = next_abs_diff
+		next_abs_diff = abs(val - buffer_start_i_1)
+	
+	return start_i
+
 
 def loop_wave(file_buffer, sample_width, num_channels, sample_rate, serial_port, frequency, phase_shift):
-	file_buffer = file_buffer.astype(np.int16).tobytes()
+	max_abs_diff = 0.005
+	
+	file_buffer_np = file_buffer.astype(np.int16)
+	file_buffer = file_buffer_np.tobytes()
 	
 	p = pyaudio.PyAudio()
 
 	stream = p.open(format=p.get_format_from_width(sample_width),
-					channels=num_channels,
-					rate=sample_rate,
-					output=True)
-					
+		channels=num_channels,
+		rate=sample_rate,
+		output=True)
+	
 	original_len = len(file_buffer) / sample_width
 	duration = original_len / sample_rate
 	
-	start_time = time()
+	start_time = int(time() / duration) * duration
 	
 	if(serial_port is not None):
 		port = serial.Serial(serial_port)
@@ -88,17 +105,17 @@ def loop_wave(file_buffer, sample_width, num_channels, sample_rate, serial_port,
 	while True:
 		time_diff = target_time - time()
 		
-		if(abs(time_diff) <= 0.005):
+		if(abs(time_diff) <= max_abs_diff):
 			stream.write(file_buffer)
 		elif(time_diff > 0):
-			extend_count = int(time_diff * sample_rate)
+			extend_count = adjust_start_i(file_buffer_np, 0, int(time_diff * sample_rate))
 			
 			if(extend_count > 0):
-				stream.write(file_buffer[- sample_width * extend_count:])
-			
+				stream.write(file_buffer[:extend_count * sample_width])
+				
 			stream.write(file_buffer)
 		else:
-			reduce_count = int(-time_diff * sample_rate)
+			reduce_count = adjust_start_i(file_buffer_np, -2, int(-time_diff * sample_rate))
 			stream.write(file_buffer[reduce_count * sample_width:])
 		
 		target_time += duration
